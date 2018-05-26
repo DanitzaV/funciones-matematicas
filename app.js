5f74ee5a8eda89ee08b7d6a47fa0fb972000d76f
viviana <dmpviviana@gmail.com>
2018-05-26 10:24:48 -0400

    resolucion de problema matematico

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//funcion que multiplica sus numeros
 function multiply(num1,num2){
     return num1 * num2;
 }
-console.log(multiply(5,6));
+// console.log(multiply(5,6));
 
+//funcion que suma sus numeros 
 function add(num1,num2){
     return num1 + num2;
 }
-console.log(add(5,6));
+// console.log(add(5,6));
+console.log(multiply(36325,add(9824,777)))
